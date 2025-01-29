4fcfa0c17d006a3cc238724ce1aeb553168c59bb
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-01-30 00:34:44 +0900

    fix: install Rspec, set Timezone

---
+++ spec/requests/application_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "application", type: :request do
+  describe "GET /up" do
+    it "works! (now write some real specs)" do
+      get rails_health_check_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
