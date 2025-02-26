b04814d4b4eeb8e7a99c4c2a222e52c9ebc8801a
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-02-26 16:34:45 +0900

    add: gem rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,10 @@ module Myapp
     config.api_only = true
 
     config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
+
     config.generators do |g|
       g.skip_routes true
       g.helper false
