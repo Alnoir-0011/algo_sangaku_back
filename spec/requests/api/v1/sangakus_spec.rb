88d8834ccc533728d3710b499b7cce4f453cbc05
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-07-25 21:17:39 +0900

    テストの追加、修正

--- spec/requests/api/v1/sangakus_spec.rb
+++ spec/requests/api/v1/sangakus_spec.rb
@@ -1,21 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Sangakus", type: :request do
-  describe "POST /sangakus" do
-    context "with_accesstoken" do
-      let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
-      let(:params) { { sangaku: attributes_for(:sangaku), fixed_inputs: [ attributes_for(:fixed_input)[:content] ] } }
-      let!(:user) { create(:user) }
+  describe "GET /sangakus/[id]" do
+    let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
+    let!(:user) { create(:user) }
+    let(:http_request) { {} }
 
-      it "return sangaku in json format" do
+    context "with_accesstoken" do
+      let!(:sangaku) { create(:sangaku, user:) }
+      let(:http_request) { get api_v1_sangaku_path(sangaku.id) }
+
+      it "return sangakus in json format" do
         authenticate_stub(user)
 
-        # post api_v1_sangakus_path, headers: headers, params: params.to_json
-        expect {
-          post api_v1_sangakus_path, headers: headers, params: params.to_json
-        }.to change(Sangaku, :count).by(1)
-        expect(response).to be_successful
+        http_request
         expect(response).to have_http_status(:ok)
+        expect(response).to be_successful
+        expect(body["data"]["attributes"]["title"]).to eq sangaku.title
+      end
+    end
+
+    context "with nonexistent id" do
+      let(:http_request) { get api_v1_sangaku_path(1000000) }
+
+      it "return 404" do
+        authenticate_stub(user)
+        http_request
+
+        expect(response).to have_http_status(:not_found)
+      expect(response).not_to be_successful
       end
     end
   end
--- spec/requests/api/v1/user/sangakus_spec.rb
+++ spec/requests/api/v1/user/sangakus_spec.rb
@@ -69,4 +69,124 @@ RSpec.describe "Api::V1::User::Sangakus", type: :request do
       end
     end
   end
+
+  describe "POST /sangakus" do
+    context "with_accesstoken" do
+      let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
+      let(:params) { { sangaku: attributes_for(:sangaku), fixed_inputs: [ attributes_for(:fixed_input)[:content] ] } }
+      let!(:user) { create(:user) }
+
+      it "success to create sangaku" do
+        authenticate_stub(user)
+
+        # post api_v1_sangakus_path, headers: headers, params: params.to_json
+        expect {
+          post api_v1_user_sangakus_path, headers: headers, params: params.to_json
+        }.to change(Sangaku, :count).by(1)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe "PATCH /user/sangakus/[id]" do
+    let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
+    let!(:user) { create(:user) }
+    let(:http_request) { {} }
+
+    context "with_accesstoken" do
+      let!(:sangaku) { create(:sangaku, title: "before_changed",  user: user) }
+      let(:http_request) { patch api_v1_user_sangaku_path(sangaku.id), headers:, params: }
+      let(:params) { { sangaku: attributes_for(:sangaku, title: "changed_title"), fixed_inputs: [ "a" ] }.to_json }
+
+      it "success to update sangaku" do
+        authenticate_stub(user)
+
+        http_request
+        expect(response).to have_http_status(:ok)
+        expect(response).to be_successful
+        expect(body["data"]["attributes"]["title"]).to eq "changed_title"
+      end
+    end
+
+    context "with nonexistent id" do
+      let(:params) { { sangaku:  attributes_for(:sangaku, title: "changed_title")  }.to_json }
+      let(:http_request) { patch api_v1_user_sangaku_path(1000000), headers:, params: }
+
+      it "return 404" do
+        authenticate_stub(user)
+        http_request
+
+        expect(response).to have_http_status(:not_found)
+        expect(response).not_to be_successful
+      end
+    end
+
+    context "with anotheruser's sangaku id" do
+      let!(:another_user) { create(:user) }
+      let!(:another_sangaku) { create(:sangaku, user: another_user) }
+      let(:params) { { sangaku:  attributes_for(:sangaku, title: "changed_title") }.to_json }
+      let(:http_request) { patch api_v1_user_sangaku_path(another_sangaku.id), headers:, params: }
+
+      it "return 404" do
+        authenticate_stub(user)
+        http_request
+
+        expect(response).to have_http_status(:not_found)
+        expect(response).not_to be_successful
+      end
+    end
+  end
+
+  describe "DELETE /sangakus/[id]" do
+    let!(:user) { create(:user) }
+    let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
+    let(:http_request) { {} }
+
+    context "with with_accesstoken" do
+      let!(:sangaku) { create(:sangaku, user:) }
+      let(:http_request) { delete api_v1_user_sangaku_path(sangaku.id), headers: }
+
+      it "return sangaku in json format" do
+        authenticate_stub(user)
+
+        expect {
+          http_request
+        }.to change(Sangaku, :count).by(-1)
+        expect(response).to have_http_status(:ok)
+        expect(response).to be_successful
+      end
+    end
+
+    context "with nonexistent id" do
+      let!(:sangaku) { create(:sangaku, user:) }
+      let(:http_request) { delete api_v1_user_sangaku_path(1000000000), headers: }
+
+      it "return 404" do
+        authenticate_stub(user)
+
+        expect {
+          http_request
+        }.to change(Sangaku, :count).by(0)
+        expect(response).to have_http_status(:not_found)
+        expect(response).not_to be_successful
+      end
+    end
+
+    context "with another_user sangaku" do
+      let!(:another_user) { create(:user, name: "another_user") }
+      let!(:another_user_sangaku) { create(:sangaku, user: another_user) }
+      let(:http_request) { delete api_v1_user_sangaku_path(another_user_sangaku), headers: }
+
+      it "return 404" do
+         authenticate_stub(user)
+
+        expect {
+          http_request
+        }.to change(Sangaku, :count).by(0)
+        expect(response).to have_http_status(:not_found)
+        expect(response).not_to be_successful
+      end
+    end
+  end
 end
