3fab30c316a183ee97c4bc80deaffaec98f76699
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-08-19 17:55:46 +0900

    add: 神社の算額一覧APIの追加

---
+++ app/controllers/api/v1/shrines_sangakus_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class ShrinesSangakusController < BaseController
+      skip_before_action :authenticate, only: %i[index]
+
+      def index
+        p search_params
+        shrine = Shrine.find(params[:shrine_id])
+        @pagy, sangakus = pagy(shrine.sangakus.search(search_params).includes(:fixed_inputs))
+        render json: SangakuSerializer.new(sangakus).serializable_hash.to_json, status: :ok
+      end
+
+      private
+
+      def search_params
+        params.permit(:title, :difficulty)
+      end
+    end
+  end
+end
