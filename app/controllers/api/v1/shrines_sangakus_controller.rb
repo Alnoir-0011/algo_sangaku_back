690433b21aee38e797568276fd18e9724e6f3822
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-08-27 16:12:04 +0900

    add:　算額の写し作成機能の実装

--- app/controllers/api/v1/shrines_sangakus_controller.rb
+++ app/controllers/api/v1/shrines_sangakus_controller.rb
@@ -4,7 +4,6 @@ module Api
       skip_before_action :authenticate, only: %i[index]
 
       def index
-        p search_params
         shrine = Shrine.find(params[:shrine_id])
         @pagy, sangakus = pagy(shrine.sangakus.search(search_params).includes(:fixed_inputs))
         render json: SangakuSerializer.new(sangakus).serializable_hash.to_json, status: :ok
