00ce85ecd8354c0842f8f9e49f6e374ffefe6b9f
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-09-14 23:25:02 +0900

    chage: ドキュメントファイルの分割, テストの追加
    rspec-openapiのconfigの追加, /upをドキュメントから削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,8 @@ Dir[Rails.root.join('spec/support/example/*.rb')].sort.each { |f| require f }
 # enable gem "webmock"
 require 'webmock'
 WebMock.allow_net_connect!(net_http_connect_on_start: true)
+
+require 'rspec_openapi'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
