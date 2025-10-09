447f82f9ad5242077666f775a751a36970bcd841
Taiki Okamoto <taiki.okamoto_0011@icloud.com>
2025-10-09 15:45:01 +0900

    add: User::SavedSangakus#answer

--- spec/requests/api/v1/user/saved_sangakus_spec.rb
+++ spec/requests/api/v1/user/saved_sangakus_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::User::SavedSangakus", type: :request, openapi: {
-  tags: %w[Api::V1::SangakuSave]
-} do
+RSpec.describe "Api::V1::User::SavedSangakus", type: :request, openapi: { tags: %w[Api::V1::SangakuSave] } do
   describe "GET /index" do
     let!(:user) { create(:user) }
     let!(:author) { create(:user, nickname: "author") }
@@ -47,6 +45,26 @@ RSpec.describe "Api::V1::User::SavedSangakus", type: :request, openapi: {
         expect(response).to have_http_status(:ok)
         expect(body["data"]["attributes"]["title"]).to eq sangaku.title
         expect(body["data"]["attributes"]["author_name"]).to eq author.nickname
+      end
+    end
+  end
+
+  describe "GET /answer" do
+    let!(:user) { create(:user) }
+    let!(:author) { create(:user, nickname: "author") }
+    let!(:sangaku) { create(:sangaku, user: author) }
+    let!(:sangaku_save_relation) { create(:user_sangaku_save, sangaku:, user: user) }
+    let!(:answer) { create(:answer, user_sangaku_save: sangaku_save_relation) }
+    let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer dummy_id_token" } }
+    let(:http_request) { get answer_api_v1_user_saved_sangaku_path(sangaku.id), headers: }
+
+    context "with access_token" do
+      it "return answer in json format" do
+        authenticate_stub(user)
+
+        http_request
+        expect(response).to have_http_status(:ok)
+        expect(body['data']['attributes']['source']).to eq answer.source
       end
     end
   end
